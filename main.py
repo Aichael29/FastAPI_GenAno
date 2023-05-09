66124c5da424df7281d9d6eaf24ed30628fe943d
Trajaa <rajaa.tiane1@gmail.com>
2023-05-09 16:58:04 +0100

    update POST-PUT-DELETE

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import random
 import csv
-from datetime import datetime, timedelta
 from fct import *
-app = FastAPI()
+from typing import List, Dict, Optional
 
+app = FastAPI()
 
 class RandomData(BaseModel):
     min_value: int
     max_value: int
     count: int
+    file_path: Optional[str] = None
 
 @app.get("/")
 async def root():
@@ -21,162 +22,103 @@ async def generate_random_data(data: RandomData):
     for _ in range(data.count):
         generated_data.append(random.randint(data.min_value, data.max_value))
     return {"data": generated_data}
-
-@app.get("/generate")
-async def generate_default_random_data():
-    generated_data = []
-    for _ in range(10):
-        generated_data.append(random.randint(0, 100))
-    return {"data": generated_data}
-
-@app.post("/generate_and_store")
-async def generate_and_store_random_data(data: RandomData):
-    generated_data = []
-    for _ in range(data.count):
-        generated_data.append(random.randint(data.min_value, data.max_value))
-
-    # Écrire les données dans un fichier CSV
-    with open('C:/Users/pc/Desktop/stage_inwi/random_data1.csv', mode='w', newline='') as csv_file:
-        fieldnames = ['data']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for data_point in generated_data:
-            writer.writerow({'data': data_point})
-
-    return {"message": "Données générées et stockées avec succès!"}
-
-""""
-exemple d'execution 
-http://127.0.0.1:8000/generate_and_store
-{
+"""cas d'utilisation : {
     "min_value": 0,
     "max_value": 100,
     "count": 5
 }"""
-
-@app.post("/generate_and_store_path_arg")
-async def generate_and_store_random_data(data: RandomData, file_path: str):
-    generated_data = []
-    for _ in range(data.count):
-        generated_data.append(random.randint(data.min_value, data.max_value))
-
-    # Écrire les données dans un fichier CSV
-    with open(file_path, mode='w', newline='') as csv_file:
-        fieldnames = ['data']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for data_point in generated_data:
-            writer.writerow({'data': data_point})
-
-    return {"message": f"Données générées et stockées avec succès dans le fichier {file_path}!"}
-
-
-"""
-exemple d'execution 
-http://localhost:8000/generate_and_store?file_path=C%3A%5CUsers%5Cpc%5CDesktop%5Cstage_inwi%5Crandom_data1.csv
-    {
-        "min_value": 1,
-        "max_value": 100,
-        "count": 10
-
-}
-"""
-class RandomData1(BaseModel):
-    min_value: int
-    max_value: int
-    count: int
-    file_path: str
-
-@app.post("/generate_and_store_path_class")
-async def generate_and_store_random_data(data: RandomData1):
-    generated_data = []
-    for _ in range(data.count):
-        generated_data.append(random.randint(data.min_value, data.max_value))
-
-    # Écrire les données dans un fichier CSV
-    with open(data.file_path, mode='w', newline='') as csv_file:
-        fieldnames = ['data']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for data_point in generated_data:
-            writer.writerow({'data': data_point})
-
-    return {"message": f"Données générées et stockées avec succès dans le fichier {data.file_path}!"}
-
-
-""" 
-exemple d'execution
-{
-        "min_value": 1,
-        "max_value": 100,
-        "count": 10,
-        "file_path":"C:/Users/pc/Desktop/stage_inwi/random_data1.csv"
-
-}
-"""
 class RandomData2(BaseModel):
     file_path: str
-@app.put("/update_csv/{line_number}/{new_value}")
-async def update_csv(path: RandomData2,line_number: int, new_value: int):
-    # Ouvrir le fichier CSV et lire les données
-    with open(path.file_path, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        data = list(csv_reader)
+    line_number: int
+    new_values: Optional[Dict[str, str]] = None
+@app.put("/update")
+def update_row(request: RandomData2):
+    with open(request.file_path, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = [row for row in reader]
 
-    # Modifier la ligne spécifiée
-    data[line_number][0] = new_value
+    if request.line_number >= len(rows):
+        return{"message":"Invalid row number"}
 
-    # Écrire les données modifiées dans le fichier CSV
-    with open(path.file_path, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(data)
+    rows[request.line_number].update(request.new_values)
 
-    return {"message": f"Ligne {line_number} modifiée avec succès!"}
+    with open(request.file_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+
+    return {"message": "Row {} updated in {}".format(request.line_number, request.file_path)}
 
 """
-exemple d'execution :http://localhost:8000/update_csv/1/42
+exemple d'execution :http://localhost:8000/update
 {
-    "file_path":"C:/Users/pc/Desktop/stage_inwi/random_data1.csv"
+    "file_path": "C:/Users/pc/Desktop/stage_inwi/random_data1.csv",
+    "line_number": 2,
+    "new_values": {
+        "id": 1999,
+        "name": "value",
+        "age": 20,
+        "salary": 12223456789
+    }
 }
 """
-@app.delete("/delete_csv/{line_number}")
-async def delete_csv(path: RandomData2,line_number: int):
-    # Ouvrir le fichier CSV et lire les données
-    with open(path.file_path, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        data = list(csv_reader)
+
+@app.delete("/delete")
+def delete_csv_row(request: RandomData2):
+    # Ouvrir le fichier CSV en mode lecture
+    with open(request.file_path, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = list(reader)
+
+    # Vérifier si l'index de ligne spécifié est valide
+    if request.line_number < 0 or request.line_number >= len(rows):
+        return{"message":"L'index de ligne spécifié n'existe pas dans le fichier CSV."}
 
     # Supprimer la ligne spécifiée
-    del data[line_number]
+    del rows[request.line_number]
 
-    # Écrire les données modifiées dans le fichier CSV
-    with open(path.file_path, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(data)
+    # Écrire les lignes mises à jour dans le fichier CSV
+    with open(request.file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(rows)
 
-    return {"message": f"Ligne {line_number} supprimée avec succès!"}
-"""exemple d'execution :http://localhost:8000/delete_csv/2
-  {
-    "file_path":"C:/Users/pc/Desktop/stage_inwi/random_data1.csv"
+    return {"message": f"Ligne {request.line_number} supprimée de {request.file_path}"}
+"""{
+    "file_path": "C:/Users/pc/Desktop/stage_inwi/random_data1.csv",
+    "line_number": 2
+}"""
 
-}
-"""
+
 class EncryptFileInput(BaseModel):
-    fichier_entree: str
-    fichier_sortie: str
-    operation: str
-    cle_chiffrement: str
-    mode_chiffrement: str
-    vecteur: str
+    fichier_entree: str=None
+    fichier_sortie: str=None
+    operation: str=None
+    cle_chiffrement: str=None
+    mode_chiffrement: str=None
+    vecteur: str=None
     colonnes: str = None
     separateur: str = None
+
+
 @app.post("/encrypt_file")
 async def encrypt_file_endpoint(input_data: EncryptFileInput):
-    df = pd.read_csv(input_data.fichier_entree, sep=input_data.separateur)
-    # Appliquer la fonction de decryptage à chaque colonne spécifiée
+    if input_data.fichier_entree is None:
+        return {"message":"enter le chemin de fichier_entree"}
+    if input_data.fichier_sortie is None:
+        return {"message":"enter le chemin de fichier_sortie"}
+    if input_data.operation is None:
+        return {"message":"enter le chemin de operation"}
+    if input_data.cle_chiffrement is None:
+        return {"message":"enter le chemin de cle_chiffrement"}
+    if input_data.mode_chiffrement is None:
+        return {"message":"enter le chemin de mode_chiffrement"}
+
+    # Lire le fichier d'entrée CSV dans un DataFrame
+    try:
+        df = pd.read_csv(input_data.fichier_entree, sep=input_data.separateur)
+    except FileNotFoundError:
+        return {"message": "Le fichier d'entrée spécifié n'existe pas."}
+    # Appliquer la fonction de cryptage ou de hachage à chaque colonne spécifiée
     if input_data.colonnes:
         colonnes = input_data.colonnes.split(',')
     else:
@@ -184,14 +126,20 @@ async def encrypt_file_endpoint(input_data: EncryptFileInput):
 
     for col in colonnes:
         if input_data.operation == 'chiffrement':
-            df[col] = df[col].apply(lambda x: aes_encrypt(str(x), input_data.cle_chiffrement, input_data.mode_chiffrement, iv=input_data.vecteur if input_data.mode_chiffrement == 'CBC' else None))
+            df[col] = df[col].apply(
+                lambda x: aes_encrypt(str(x), input_data.cle_chiffrement, input_data.mode_chiffrement,
+                                      iv=input_data.vecteur if input_data.mode_chiffrement == 'CBC' else None))
         elif input_data.operation == 'dechiffrement':
-            df[col] = df[col].apply(lambda x: aes_decrypt(str(x), input_data.cle_chiffrement, input_data.mode_chiffrement, iv=input_data.vecteur if input_data.mode_chiffrement == 'CBC' else None))
+            df[col] = df[col].apply(
+                lambda x: aes_decrypt(str(x), input_data.cle_chiffrement, input_data.mode_chiffrement,
+                                      iv=input_data.vecteur if input_data.mode_chiffrement == 'CBC' else None))
         elif input_data.operation == 'hashage':
             df[col] = df[col].apply(lambda x: sha256_hash(str(x)))
-    # écrire le dataframe modifié dans un nouveau fichier excel
+
+    # Écrire le dataframe modifié dans un nouveau fichier csv
     df.to_csv(input_data.fichier_sortie, index=False, sep=input_data.separateur)
 
+    return {"message": "Le traitement du fichier a été effectué avec succès."}
 
 """ exemple d'execution :  http://localhost:8000/encrypt_file
 {
@@ -206,29 +154,44 @@ async def encrypt_file_endpoint(input_data: EncryptFileInput):
 
 }"""
 
-@app.post("/generate_data_int_str_date")
-async def generate_data(random_data: RandomData):
-    data = []
-    for i in range(random_data.count):
-        # Générer une valeur aléatoire de type int
-        int_val = random.randint(random_data.min_value, random_data.max_value)
 
-        # Générer une valeur aléatoire de type str
-        str_val = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=random.randint(1, 10)))
 
-        # Générer une valeur aléatoire de type date
-        date_val = datetime.today() - timedelta(days=random.randint(0, 365))
+class RandomDataGenerator:
+    def __init__(self, column_names: List[str], column_types: Dict[str, str]):
+        self.column_names = column_names
+        self.column_types = column_types
 
-        # Ajouter les valeurs dans la liste de données
-        data.append((int_val, str_val, date_val.strftime("%Y-%m-%d")))
+    def generate_random_data(self, num_rows: int, file_path: str) -> None:
+        if file_path is None:
+            file_path="C:/Users/pc/Desktop/stage_inwi/random_data.csv"
+        with open(file_path, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.column_names)
+            writer.writeheader()
+            for i in range(num_rows):
+                row = {}
+                for column_name, column_type in self.column_types.items():
+                    if column_type == "int":
+                        row[column_name] = random.randint(0, 100)
+                    elif column_type == "float":
+                        row[column_name] = random.uniform(0, 100)
+                    elif column_type == "str":
+                        row[column_name] = ''.join(random.choices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], k=10))
+                    else:
+                        raise ValueError("Invalid column type: {}".format(column_type))
+                writer.writerow(row)
 
-    # Écrire les données dans un fichier CSV
-    with open('random_data.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['int', 'str', 'date'])
-        writer.writerows(data)
 
-    return {"message": f"Les données ont été générées et écrites dans le fichier 'random_data.csv'."}
+class RandomDataRequest(BaseModel):
+    num_rows: int
+    file_path: Optional[str] = None
+    column_names: List[str]
+    column_types: Dict[str, str]
+
+@app.post("/generate_random_data")
+def generate_random_data(request: RandomDataRequest):
+    generator = RandomDataGenerator(request.column_names, request.column_types)
+    generator.generate_random_data(request.num_rows, request.file_path)
+    return {"message": "Data generated and saved to {}".format(request.file_path)}
 
 
 
